a8f412582b6c508780ca93739054c0627388a546
Ben Pascoe <garthwembury@hotmail.com>
2019-08-12 21:56:25 +0100

    Update CandleSimulation.py

--- 07-Candle-Simulation-on-RGB-LED/CandleSimulation.py
+++ 07-Candle-Simulation-on-RGB-LED/CandleSimulation.py
@@ -6,6 +6,9 @@ unicorn.set_layout(unicorn.AUTO)
 unicorn.rotation(0)
 width,height=unicorn.get_shape()
 
+def rand_flicker_sleep():
+    time.sleep(random.randint(3,10) / 100.0)
+
 while True:
     x = randint(0, (width-1))
     y = randint(0, (height-1))
@@ -17,6 +20,3 @@ while True:
     unicorn.set_pixel(x, y, r, g, b)
     unicorn.show()
     rand_flicker_sleep()
-
-def rand_flicker_sleep():
-    time.sleep(random.randint(3,10) / 100.0)
